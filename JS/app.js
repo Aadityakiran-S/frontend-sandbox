892e1568fa819a7315ad0a3f258156e27993fdf0
Aadityakiran S <aadityakirans@gmail.com>
2023-03-02 08:30:54 +0530

    Learnt some JS

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+const firstResult = addTwoNumbers(5, 5);
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+
+const seondResult = add(5, 5);
+
+console.log(firstResult, add(5, 5));
