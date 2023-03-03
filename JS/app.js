87b6f021244c2ba94a99e6865100b61a4e504347
Aadityakiran S <aadityakirans@gmail.com>
2023-03-03 09:30:15 +0530

    filter expression in JS

--- JS/app.js
+++ JS/app.js
@@ -5,23 +5,12 @@ const people = [
     { name: 'anna', age: 21, profession: 'HR' }
 ]
 
-const ages = people.map((person) => {
-    return person.age;
-});
-console.log(ages);
-
-const names = people.map((person) => {
-    return {
-        name: person.name.toUpperCase(),
-        age: person.age
-    }
+const youngPeople = people.filter((person) => {
+    return person.age <= 25;
 })
-console.log(names);
+console.log(youngPeople);
 
-const headings = people.map((person) => {
-    return `<h1>${person.name}</h1>`;
+const developers = people.filter((person) => {
+    return person.profession === 'senior developer';
 })
-
-document.body.innerHTML = headings.join('');
-
-console.log(headings);
+console.log(developers);
