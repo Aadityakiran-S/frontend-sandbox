fcfac744d4c011c5885cf0de4dd1c4b3fd467780
Aadityakiran S <aadityakirans@gmail.com>
2023-03-03 09:46:06 +0530

    find and it's differences with filter

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,16 @@
 const people = [
-    { name: 'bob', age: 20, profession: 'developer' },
-    { name: 'peter', age: 25, profession: 'designer' },
-    { name: 'susy', age: 30, profession: 'the boss' },
-    { name: 'anna', age: 21, profession: 'HR' }
+    { name: 'bob', age: 20, profession: 'developer', id: 1 },
+    { name: 'peter', age: 25, profession: 'designer', id: 2 },
+    { name: 'susy', age: 30, profession: 'the boss', id: 3 },
+    { name: 'anna', age: 21, profession: 'HR', id: 4 }
 ]
 
-const youngPeople = people.filter((person) => {
-    return person.age <= 25;
+const person = people.find((p) => {
+    return p.id === 1;
 })
-console.log(youngPeople);
+console.log(person); //Returns single object
 
-const developers = people.filter((person) => {
-    return person.profession === 'senior developer';
+const filteredPerson = people.filter((person) => {
+    return person.id === 1;
 })
-console.log(developers);
+console.log(filteredPerson); //Returns array
