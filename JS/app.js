c93d7bf845d83210eb0023caeb916e6fadb96a35
Aadityakiran S <aadityakirans@gmail.com>
2023-03-03 09:25:02 +0530

    Learnt about map function in JS

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,27 @@
 const people = [
     { name: 'bob', age: 20, profession: 'developer' },
     { name: 'peter', age: 25, profession: 'designer' },
-    { name: 'susy', age: 30, profession: 'the boss' }
+    { name: 'susy', age: 30, profession: 'the boss' },
+    { name: 'anna', age: 21, profession: 'HR' }
 ]
 
-function showPerson(person) {
-    console.log(person.profession.toUpperCase());
-}
+const ages = people.map((person) => {
+    return person.age;
+});
+console.log(ages);
 
-people.forEach(showPerson);
+const names = people.map((person) => {
+    return {
+        name: person.name.toUpperCase(),
+        age: person.age
+    }
+})
+console.log(names);
+
+const headings = people.map((person) => {
+    return `<h1>${person.name}</h1>`;
+})
+
+document.body.innerHTML = headings.join('');
+
+console.log(headings);
