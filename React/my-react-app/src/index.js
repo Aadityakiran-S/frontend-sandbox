e4f20e77c4e05148bfa6adc3e6b517f678422c9e
AadityakiranS <aadityakirans@gmail.com>
2023-03-07 15:48:25 +0530

    JSX in React

--- React/my-react-app/src/index.js
+++ React/my-react-app/src/index.js
@@ -3,7 +3,23 @@ import ReactDom from 'react-dom';
 
 //Must capitalize functions for React to know it's supposed to be used
 function Greeting() {
-  return <h1>Hello there!</h1>
+  return (
+    <div>
+      <h3>Hello World</h3>
+      <ul>
+        <li>
+          <a href="#">hello world</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
+
+// const Greeting = () => {
+//   return React.createElement
+//     ('div',
+//       {},
+//       React.createElement('h1', {}, 'Hello World'));
+// }
 
 ReactDom.render(<Greeting></Greeting>, document.getElementById('root'));
