7bd310c9e614ad2ccce6ff817e1a6c06af9c6eda
Aadityakiran S <aadityakirans@gmail.com>
2023-03-10 09:34:53 +0530

    nextSibling and nextElemnetSibling

--- VanilaJS/JS/app.js
+++ VanilaJS/JS/app.js
@@ -1,5 +1,18 @@
-const result = document.querySelector('#result');
-result.style.backgroundColor = 'red';
+const first = document.querySelector('.first');
+first.style.color = 'red';
 
-const item = document.querySelector('.special');
-console.log(item);
+//This is just going to be a whitespace
+const whiteSpaceBeforeSecond = first.nextSibling;
+console.log(typeof (whiteSpaceBeforeSecond));
+console.log(whiteSpaceBeforeSecond);
+
+//This is gonna give us the correct next sibling
+const second = first.nextSibling.nextSibling;
+console.log(typeof (second));
+console.log(second);
+second.style.color = 'blue';
+
+//This will also do the same
+const sameAsAbove = first.nextElementSibling;
+console.log(typeof (sameAsAbove));
+console.log(sameAsAbove);
