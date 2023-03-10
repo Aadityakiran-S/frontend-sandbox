6d467e71f7ad01c49d00d229cd4dd6456f0bd4f3
Aadityakiran S <aadityakirans@gmail.com>
2023-03-10 10:36:53 +0530

    nodeValue and textContent

--- VanilaJS/JS/app.js
+++ VanilaJS/JS/app.js
@@ -1,18 +1,7 @@
-const first = document.querySelector('.first');
-first.style.color = 'red';
+const item = document.getElementById('special');
 
-//This is just going to be a whitespace
-const whiteSpaceBeforeSecond = first.nextSibling;
-console.log(typeof (whiteSpaceBeforeSecond));
-console.log(whiteSpaceBeforeSecond);
+const value = item.firstChild.nodeValue;
+console.log(value);
 
-//This is gonna give us the correct next sibling
-const second = first.nextSibling.nextSibling;
-console.log(typeof (second));
-console.log(second);
-second.style.color = 'blue';
-
-//This will also do the same
-const sameAsAbove = first.nextElementSibling;
-console.log(typeof (sameAsAbove));
-console.log(sameAsAbove);
+const easyValue = item.textContent;
+console.log(easyValue);
