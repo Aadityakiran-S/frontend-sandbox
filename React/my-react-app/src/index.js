e4eda65fd3edd4f294519d480e519c3966e8f280
AadityakiranS <aadityakirans@gmail.com>
2023-03-14 16:15:03 +0530

    props and children in props

--- React/my-react-app/src/index.js
+++ React/my-react-app/src/index.js
@@ -1,34 +1,51 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 //CSS
 import './index.css'; //Just grabbing everything 
 
-const title = `I love you to the moon and back`;
-const author = `Amelia Hepworth`;
-const imageURL = `https://m.media-amazon.com/images/I/91hIsjFuICL.jpg`;
+//variables
+const firstBook = {
+  title: `I love you to the moon and back`,
+  author: `Amelia Hepworth`,
+  image: `https://m.media-amazon.com/images/I/91hIsjFuICL.jpg`
+}
+const secondBook = {
+  title: "Grandma's bag of stories",
+  author: "Sudha Murthy",
+  image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61yB0UFlM3L._SY344_BO1,204,203,200_.jpg"
+}
 
 const BookList = () => {
   return (
     <section className='bookList'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      {/* FIrst book */}
+      <Book
+        image={firstBook.image}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>This is a goddamn description that I am having to write for this book since I have to be "original" afterall.</p>
+      </Book>
+      {/* Second book */}
+      <Book
+        image={secondBook.image}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { image, title, author, children } = props;
   return (
     <article className='book '>
       <h1>{title.toUpperCase()}</h1>
-      <img src={imageURL} width={'50%'} alt="image" />
-      <br />
+      <img src={image} width={'50%'} alt="image" />
+      {/* <p>{children}</p> */}
+      {children}
       <div>
         <p style={{ display: 'inline-block', marginRight: '0.25em', fontStyle: 'italic' }}>by</p>
         <div style={{ display: 'inline-block' }}>
@@ -39,4 +56,6 @@ const Book = () => {
 }
 
 //injecting to index.html
-ReactDom.render(<BookList />, document.getElementById('root'));
+const root = document.getElementById('root');
+createRoot(root).render(<BookList />)
+// ReactDom.render(<BookList />, document.getElementById('root'));
