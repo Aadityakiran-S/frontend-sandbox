ae3c03b6483936c71799d5fbf3834abf29643ff7
AadityakiranS <aadityakirans@gmail.com>
2023-03-14 15:04:44 +0530

    Did some cleanup

--- React/my-react-app/src/index.js
+++ React/my-react-app/src/index.js
@@ -4,6 +4,10 @@ import ReactDom from 'react-dom';
 //CSS
 import './index.css'; //Just grabbing everything 
 
+const title = `I love you to the moon and back`;
+const author = `Amelia Hepworth`;
+const imageURL = `https://m.media-amazon.com/images/I/91hIsjFuICL.jpg`;
+
 const BookList = () => {
   return (
     <section className='bookList'>
@@ -22,32 +26,17 @@ const BookList = () => {
 const Book = () => {
   return (
     <article className='book '>
-      <Title />
-      <Image />
+      <h1>{title}</h1>
+      <img src={imageURL} width={'50%'} alt="image" />
       <br />
-      {/* By that Author, pretty hard to do but useless */}
       <div>
         <p style={{ display: 'inline-block', marginRight: '0.25em', fontStyle: 'italic' }}>by</p>
-        <div style={{ display: 'inline-block' }}><Author /></div>
+        <div style={{ display: 'inline-block' }}>
+          <h4>{author}</h4></div>
       </div>
     </article>
   );
 }
-
-const Image = () => {
-  return (
-    <img src="https://m.media-amazon.com/images/I/91hIsjFuICL.jpg" width={'50%'} alt="book image" />
-  );
-}
-
-const Title = () => <h1>I love you to the moon and back</h1>;
-const Author = () => <h4
-  style={
-    {
-      color: '#617d98',
-      fontSize: '1rem',
-      marginTop: '0.25rem'
-    }}>Amelia Hepworth</h4>;
 
 //injecting to index.html
 ReactDom.render(<BookList />, document.getElementById('root'));
