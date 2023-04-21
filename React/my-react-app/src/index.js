0960beabddf4c4fc5ad7dc501773698a9fe4c984
AadityakiranS <aadityakirans@gmail.com>
2023-04-21 16:11:08 +0530

    Started new Project in React 18

--- React/my-react-app/src/index.js
+++ React/my-react-app/src/index.js
@@ -1,56 +1,17 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-//CSS
-import './index.css'; //Just grabbing everything 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-//variables
-const books = [
-  {
-    id: 1,
-    title: `I love you to the moon and back`,
-    author: `Amelia Hepworth`,
-    image: `https://m.media-amazon.com/images/I/91hIsjFuICL.jpg`
-  },
-  {
-    id: 2,
-    title: "Grandma's bag of stories",
-    author: "Sudha Murthy",
-    image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/61yB0UFlM3L._SY344_BO1,204,203,200_.jpg"
-  },
-  {
-    id: 3,
-    title: "The Vanishing Half",
-    author: "Brit Bennett ",
-    image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T2/images/I/41tesqOC72L._AC_UY218_.jpg",
-  },
-]
-
-const BookList = () => {
-  return (
-    <section className='bookList'>
-      {books.map((book) => {
-        return (<Book key={book.id} {...book}></Book>);
-      })}
-    </section>
-  );
-}
-
-const Book = (props) => {
-  const { image, title, author } = props;
-  return (
-    <article className='book '>
-      <h1>{title.toUpperCase()}</h1>
-      <img src={image} width={'50%'} alt="image" />
-      <div>
-        <p style={{ display: 'inline-block', marginRight: '0.25em', fontStyle: 'italic' }}>by</p>
-        <div style={{ display: 'inline-block' }}>
-          <h4>{author}</h4></div>
-      </div>
-    </article>
-  );
-}
-
-//injecting to index.html
-const root = document.getElementById('root');
-createRoot(root).render(<BookList />);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
