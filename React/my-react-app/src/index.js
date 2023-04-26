62e6e6c51192c135bcb38ae55309965b9aef90aa
Aadityakiran S <aadityakirans@gmail.com>
2023-04-26 10:33:45 +0530

    Learnt Create Element

--- React/my-react-app/src/index.js
+++ React/my-react-app/src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-    return <h1>My first component</h1>;
+    return (
+        <div>
+            <h1>My first component</h1>
+        </div>
+    );
 }
 
+// function Greeting() {
+//     return React.createElement(
+//         'div',
+//         {},
+//         React.createElement('h1', {}, 'Hello World')
+//     );
+// }
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<Greeting />)
