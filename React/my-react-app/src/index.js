383c6c9edce99c313f8be3f3646b30a5f9c29d4e
Aadityakiran S <aadityakirans@gmail.com>
2023-07-10 14:29:03 +0530

    My first component

--- React/my-react-app/src/index.js
+++ React/my-react-app/src/index.js
@@ -2,29 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 function Greeting() {
-    return (
-        <>
-            <div>
-                <h3>My first component</h3>
-                <ul>
-                    <li>
-                        <a href="#">Hello World</a>
-                    </li>
-                </ul>
-            </div>
-            <h2>My second heading</h2>
-        </>
-    );
+    return <h2>My First Component</h2>;
 }
 
-// function Greeting() {
-//     return React.createElement(
-//         'div',
-//         {},
-//         React.createElement('h1', {}, 'Hello World')
-//     );
-// }
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(<Greeting />)
+root.render(<Greeting />);
